34c767456968781f7de81fe85ea762d4f469fe59
sickventures <69673432+sickventures@users.noreply.github.com>
2021-11-06 21:22:30 +0100

    db 3

--- web/settings.py
+++ web/settings.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -130,7 +130,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
