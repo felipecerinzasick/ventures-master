73f597a9a3121c355fcf99adbbcc203737e0e7cc
felipecerinzasick <35343980+felipecerinzasick@users.noreply.github.com>
2023-11-07 19:49:33 +0100

    Update forms.py
    Add ContactForm class and import Contact from models.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
 from .models import Post
+from .models import Contact
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'message']
+        
 
 class PostForm(forms.ModelForm):
     class Meta:
