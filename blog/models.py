6d093b2f23430cdf8ac9dd6d130e8d0741b6fcec
felipecerinzasick <35343980+felipecerinzasick@users.noreply.github.com>
2024-02-11 18:28:36 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,16 @@ from django.conf import settings
 from ckeditor_uploader.fields import RichTextUploadingField
 
 
+class Resource(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    link = models.URLField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+        
+
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
