8a06de0d45c960f785ceda0b6fc79384882a87d8
felipecerinzasick <35343980+felipecerinzasick@users.noreply.github.com>
2024-02-26 14:20:52 +0100

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -3,6 +3,9 @@ from django import forms
 from .models import Post
 from .models import Contact
 
+from .models import BitcoinWallet  # Ensure this import is correct
+
+
 class BitcoinWalletForm(forms.ModelForm):
     class Meta:
         model = BitcoinWallet
